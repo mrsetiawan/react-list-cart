a33e1bb185f5113d95e9e116e49f9cf251c40994
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-05-11 23:21:19 +0700

    add redux dev tools extentions

--- src/config/store/store.js
+++ src/config/store/store.js
@@ -1,13 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxLogger from 'redux-logger';
 import reduxThunk from 'redux-thunk';
 import rootReducers from '../reducers/main';
 
-const middelwares = applyMiddleware(reduxLogger, reduxThunk);
-
 const store = createStore(
   rootReducers,
-  middelwares
+  compose(
+    applyMiddleware(
+      reduxLogger, reduxThunk
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
